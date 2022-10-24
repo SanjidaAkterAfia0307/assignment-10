2141503060531a7a03363ba2f8ddd06536834447
Afia <sanjidaakterafia0307@gmail,com>
2022-10-25 00:56:57 +0600

    components set and header

---
+++ src/Routes/Router/Router.js
@@ -0,0 +1,37 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../layout/Main";
+import Home from "../../components/Home/Home"
+import Courses from "../../components/Courses/Courses"
+import Blog from "../../components/Blog/Blog";
+import LogIn from "../../components/LogIn/LogIn";
+import Register from "../../components/Register/Register";
+
+
+export const router=createBrowserRouter([
+    {
+        path:"/",
+        element:<Main></Main>,
+        children:[
+            {
+                path:"/",
+                element:<Home></Home>
+            },
+            {
+                path:"/courses",
+                element:<Courses></Courses>
+            },
+            {
+                path:"/blog",
+                element:<Blog></Blog>
+            },
+            {
+                path:"/login",
+                element:<LogIn></LogIn>
+            },
+            {
+                path:"/register",
+                element:<Register></Register>
+            },
+        ]
+    }
+])
